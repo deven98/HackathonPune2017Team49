8825235cfb5ccb9a7955f54b7b41a4156779c096
ShubhamPurandare <shubham.purandare@gmail.com>
2017-09-23 11:30:32 +0530

    Initial Activities created

--- app/src/main/java/devapp/com/hackathonpune2017team49/DecidingActivity.java
+++ app/src/main/java/devapp/com/hackathonpune2017team49/DecidingActivity.java
@@ -3,11 +3,15 @@ package devapp.com.hackathonpune2017team49;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DecidingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
---
+++ app/src/main/java/devapp/com/hackathonpune2017team49/Manager/AssignTaskActivity.java
@@ -0,0 +1,15 @@
+package devapp.com.hackathonpune2017team49.Manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import devapp.com.hackathonpune2017team49.R;
+
+public class AssignTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assign_task);
+    }
+}
