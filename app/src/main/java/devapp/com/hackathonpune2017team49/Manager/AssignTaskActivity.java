9542ff9fdc500c3c77c80489cb891a41d969d3c6
Deven <deven9852@gmail.com>
2017-09-23 16:19:47 +0530

    Added Search in add task.

--- app/src/main/java/devapp/com/hackathonpune2017team49/Manager/AssignTaskActivity.java
+++ app/src/main/java/devapp/com/hackathonpune2017team49/Manager/AssignTaskActivity.java
@@ -2,14 +2,56 @@ package devapp.com.hackathonpune2017team49.Manager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 import devapp.com.hackathonpune2017team49.R;
 
 public class AssignTaskActivity extends AppCompatActivity {
 
+    String place;
+    String specifics;
+    String phoneNumber;
+    String time;
+
+    EditText nameEditText;
+    TextView addressTextView;
+    Button submitButton;
+
+    void initialize(){
+
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign_task);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i("TAG", "Place: " + place);
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("TAG", "An error occurred: " + status);
+            }
+        });
     }
 }
