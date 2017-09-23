5f5a09f1ab2451d6a97aff11d4469186d3a58a80
Deven <deven9852@gmail.com>
2017-09-23 12:19:09 +0530

    Changed mainActivity name

--- app/src/main/java/devapp/com/hackathonpune2017team49/DecidingActivity.java
+++ app/src/main/java/devapp/com/hackathonpune2017team49/DecidingActivity.java
@@ -3,23 +3,12 @@ package devapp.com.hackathonpune2017team49;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import io.realm.Realm;
-
 public class DecidingActivity extends AppCompatActivity {
-
-    Realm realm;
-    EmpDatabase database ;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        realm = Realm.getDefaultInstance();
-        database = realm.where(EmpDatabase.class).findFirst();
-
-
-
+        setContentView(R.layout.activity_deciding);
 
 
 
